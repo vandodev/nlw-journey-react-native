abc07ed37b1267bba09a320fe259b87dee4160f7
vandodev <vandoaparecido@hotmail.com>
2024-07-08 21:49:14 -0300

    :airplane: :ship: :car: Tipos de estilização

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,15 @@
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 export default function index(){
     return(
-        <View>
+        // <View style={{flex:1, backgroundColor:"gray", justifyContent:"center"}}>
+        <View style={styles.container}>
             <Text>Olá mundo</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1, backgroundColor:"gray", justifyContent:"center"
+    }
+})
