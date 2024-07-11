a8e668ae1eaadd19d02abd3f596fc715248e4ab8
vandodev <vandoaparecido@hotmail.com>
2024-07-11 08:36:04 -0300

    :airplane: :ship: :car: Buscando viagem pelo id

--- mobile/src/app/trip/[id].tsx
+++ mobile/src/app/trip/[id].tsx
@@ -1,6 +1,51 @@
+import { useEffect, useState } from "react"
 import {  View } from "react-native"
+import { router, useLocalSearchParams } from "expo-router"
+import { TripDetails, tripServer } from "@/server/trip-server"
+import { Loading } from "@/components/loading"
+
+export type TripData = TripDetails & { when: string }
 
 export default function Trip() {
+  // const tripId = useLocalSearchParams()
+  // const tripId = useLocalSearchParams().id
+  // const tripId = useLocalSearchParams<{id: string}>().id
+  // console.log(tripId)
+
+  const tripId = useLocalSearchParams<{id: string}>().id
+  
+  // LOADING
+  const [isLoadingTrip, setIsLoadingTrip] = useState(true)
+
+  // DATA
+  const [tripDetails, setTripDetails] = useState({} as TripData)
+
+  async function getTripDetails() {
+    try {
+      setIsLoadingTrip(true)
+        
+      if(!tripId){
+        return router.back()
+      } 
+        
+      const trip = await tripServer.getById(tripId)
+      console.log(trip)
+        
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoadingTrip(false)
+    }
+  }
+
+  useEffect(() => {
+    getTripDetails()
+  }, [])
+
+  if (isLoadingTrip) {
+  return <Loading />
+  }
+
   return (
     <View className="flex-1 px-5 pt-16">
       
