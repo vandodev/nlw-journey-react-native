e8043c7818f157ceb44352c18143ce01dbc3a2b1
vandodev <vandoaparecido@hotmail.com>
2024-07-11 09:51:56 -0300

    :airplane: :ship: :car: Criando menu

--- mobile/src/app/trip/[id].tsx
+++ mobile/src/app/trip/[id].tsx
@@ -4,10 +4,13 @@ import { router, useLocalSearchParams } from "expo-router"
 import { TripDetails, tripServer } from "@/server/trip-server"
 import { Loading } from "@/components/loading"
 import { Input } from "@/components/input"
+import { Button } from "@/components/button"
 
 import {
   MapPin,
   Settings2,
+  Info,
+  CalendarRange,
 } from "lucide-react-native"
 
 import dayjs from "dayjs"
@@ -25,6 +28,8 @@ export default function Trip() {
 
   // DATA
   const [tripDetails, setTripDetails] = useState({} as TripData)
+  
+  const [option, setOption] = useState<"activity" | "details">("activity")
   const [destination, setDestination] = useState("")
 
   async function getTripDetails() {
@@ -82,6 +87,36 @@ export default function Trip() {
           <Settings2 color={colors.zinc[400]} size={20} />
         </TouchableOpacity>
       </Input>
+
+      <View className="w-full absolute -bottom-1 self-center justify-end pb-5 z-10 bg-zinc-950">
+        <View className="w-full flex-row bg-zinc-900 p-4 rounded-lg border border-zinc-800 gap-2">
+          <Button
+            className="flex-1"
+            onPress={() => setOption("activity")}
+            variant={option === "activity" ? "primary" : "secondary"}
+          >
+            <CalendarRange
+              color={
+                option === "activity" ? colors.lime[950] : colors.zinc[200]
+              }
+              size={20}
+            />
+            <Button.Title>Atividades</Button.Title>
+          </Button>
+
+          <Button
+            className="flex-1"
+            onPress={() => setOption("details")}
+            variant={option === "details" ? "primary" : "secondary"}
+          >
+            <Info
+              color={option === "details" ? colors.lime[950] : colors.zinc[200]}
+              size={20}
+            />
+            <Button.Title>Detalhes</Button.Title>
+          </Button>
+        </View>
+      </View>
     </View>
   )
 }
