21167723dbb6d16d51857c734b15cba8a35864a4
vandodev <vandoaparecido@hotmail.com>
2024-07-12 10:20:47 -0300

    :airplane: :ship: :car: Estilizando botão e testo

--- mobile/src/app/trip/activities.tsx
+++ mobile/src/app/trip/activities.tsx
@@ -1,5 +1,10 @@
 import { Text, View } from "react-native";
 import { TripData } from "./[id]"
+import { Button } from "@/components/button"
+import {
+  PlusIcon,
+} from "lucide-react-native"
+import { colors } from "@/styles/colors"
 
 type Props = {
     tripDetails: TripData
@@ -9,7 +14,17 @@ export function Activities({ tripDetails }: Props) {
   
   return (
     <View className="flex-1">
-        <Text className="text-white">{ tripDetails.destination}</Text>
+
+      <View className="w-full flex-row mt-5 mb-6 items-center">
+        <Text className="text-zinc-50 text-2xl font-semibold flex-1">
+          Atividades
+        </Text>
+
+        <Button>
+          <PlusIcon color={colors.lime[950]} />
+          <Button.Title>Nova atividade</Button.Title>
+        </Button>
+      </View>
     </View>
   )
 }
